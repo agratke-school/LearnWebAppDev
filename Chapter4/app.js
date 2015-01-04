ba4b5d059895ee0687e86cdae750de1459c32b73
Ken McLeod <admin@therinken.com>
2015-01-04 11:41:55 -0600

    editing out redundencies in js file

--- Chapter4/app.js
+++ Chapter4/app.js
@@ -1,6 +1,7 @@
 var main = function() {
     "use strict";
-    $(".comment-input button").on("click", function(event) {
+
+    var addCommentFromInputBox = function() {
         var $new_comment;
 
         if ($(".comment-input input").val() !== "") {
@@ -10,24 +11,16 @@ var main = function() {
             $new_comment.fadeIn();
             $(".comment-input input").val("");
         } else {
-        	alert("Enter some text!")
+            alert("Enter some text!")
         }
+    }
+    $(".comment-input button").on("click", function(event) {
+        addCommentFromInputBox();
     })
     $(".comment-input input").on("keypress", function(event) {
-    	var $new_comment;
-
-    	if(event.keyCode === 13){
-    		if ($(".comment-input input").val() !== "") {
-            var $new_comment = $("<p>").text($(".comment-input input").val());
-
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("");
-        } else {
-        	alert("Enter some text!")
+        if (event.keyCode === 13) {
+            addCommentFromInputBox();
         }
-    	}
     })
 
 };
