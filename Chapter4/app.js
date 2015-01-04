c5d971c645d763840cb54148435bcdc727878313
Ken McLeod <admin@therinken.com>
2015-01-04 11:33:24 -0600

    adding some more jQuery sweetness to the mix

--- Chapter4/app.js
+++ Chapter4/app.js
@@ -1,11 +1,35 @@
 var main = function() {
     "use strict";
-    $(".comment-input button").on("click", function(event){
-    	var $new_comment = $("<p>").text($(".comment-input input").val());
+    $(".comment-input button").on("click", function(event) {
+        var $new_comment;
 
-    	$(".comments").append($new_comment);
+        if ($(".comment-input input").val() !== "") {
+            var $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment.hide();
+            $(".comments").append($new_comment);
+            $new_comment.fadeIn();
+            $(".comment-input input").val("");
+        } else {
+        	alert("Enter some text!")
+        }
     })
-    
+    $(".comment-input input").on("keypress", function(event) {
+    	var $new_comment;
+
+    	if(event.keyCode === 13){
+    		if ($(".comment-input input").val() !== "") {
+            var $new_comment = $("<p>").text($(".comment-input input").val());
+
+            $new_comment.hide();
+            $(".comments").append($new_comment);
+            $new_comment.fadeIn();
+            $(".comment-input input").val("");
+        } else {
+        	alert("Enter some text!")
+        }
+    	}
+    })
+
 };
 
 $(document).ready(main);
