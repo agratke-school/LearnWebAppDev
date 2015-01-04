1336061392de9e63c52c598f484b7bad17068c89
Ken McLeod <admin@therinken.com>
2015-01-04 15:41:05 -0600

    changed dir name from chapter to prject base

---
+++ Commentor/app.js
@@ -0,0 +1,28 @@
+var main = function() {
+    "use strict";
+
+    var addCommentFromInputBox = function() {
+        var $new_comment;
+
+        if ($(".comment-input input").val() !== "") {
+            var $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment.hide();
+            $(".comments").append($new_comment);
+            $new_comment.fadeIn();
+            $(".comment-input input").val("");
+        } else {
+            alert("Enter some text!")
+        }
+    }
+    $(".comment-input button").on("click", function(event) {
+        addCommentFromInputBox();
+    })
+    $(".comment-input input").on("keypress", function(event) {
+        if (event.keyCode === 13) {
+            addCommentFr omInputBox();
+        }
+    })
+
+};
+
+$(document).ready(main);
